3696b03cd170906a098a74cd58a63e80bae4d92a
hedmi-san <88062690+hedmi-san@users.noreply.github.com>
2024-05-05 16:33:45 +0100

    sixth commit

---
+++ lib/classes/orders.dart
@@ -0,0 +1,8 @@
+class Order {
+  final String name;
+  final int quantity;
+  final String recieverName;
+  final DateTime time;
+
+  Order(this.name, this.quantity, this.recieverName, this.time);
+}
